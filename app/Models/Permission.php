17d19513f32965951c341547164e3106709f7a55
Liviu Andrei <liviuandrei.dev@gmail.com>
2021-08-25 16:02:38 +0300

    Add Permission files
    	- files:
    		-> model: Permission
    			+ added relationships (Many-to-Many) to Roles model
    		-> controller: PermissionController
    			+ added empty CRUD functions that will be used in the future
    		-> migration: create_permissions_table
    			+ added DB columns
    		-> seeder: PermissionSeeder

---
+++ app/Models/Permission.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Permission extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name'
+    ];
+
+    /**
+     * The roles that belong to the role.
+     */
+    public function roles()
+    {
+        return $this->belongsToMany(Role::class, 'role_permission');
+    }
+}
