049fbcec84a181e256eacdebc0039234f49371bd
Liviu Andrei <liviuandrei.dev@gmail.com>
2021-08-25 16:06:05 +0300

    Add Many-to-Many relationship migration files
    	+ Relationships between Role-Permission and User-Role models

---
+++ database/migrations/2021_08_25_124043_create_role_permission_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRolePermissionTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('role_permission', function (Blueprint $table) {
+            $table->foreignId('role_id')->constrained()->onDelete('cascade');
+            $table->foreignId('permission_id')->constrained()->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('role_permission', function (Blueprint $table) {
+            //
+        });
+    }
+}
