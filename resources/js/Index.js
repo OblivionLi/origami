0f32dd3e5bdafb83fb7ac907bba6210f0d207144
Liviu Andrei <liviuandrei.dev@gmail.com>
2021-09-07 17:18:27 +0300

    Add main react file
    	+ this file will contain all the react routes for all files that exist or will exist in the future
    	+ acts like an api.php or web.php in laravel (a place for routes)

---
+++ resources/js/Index.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from "react-redux";
+import store from "./store";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import HomeScreen from './screens/HomeScreen';
+import LoginScreen from './screens/public/users/LoginScreen';
+import RegisterScreen from './screens/public/users/RegisterScreen';
+import ResetPasswordScreen from './screens/public/users/ResetPasswordScreen';
+import ForgotPasswordScreen from './screens/public/users/ForgotPasswordScreen';
+import SettingsScreen from './screens/public/users/SettingsScreen';
+
+function Index() {
+    return (
+        <Router>
+            <Switch>
+                {/* Users */}
+                <Route path="/" component={HomeScreen} exact />
+                <Route path="/login" component={LoginScreen} />
+                <Route path="/register" component={RegisterScreen} />
+                <Route path="/reset-password/:id" component={ResetPasswordScreen} />
+                <Route path="/forgot-password" component={ForgotPasswordScreen} />
+                <Route path="/settings" component={SettingsScreen} />
+            </Switch>
+        </Router>
+    );
+}
+
+export default Index;
+
+if (document.getElementById("app")) {
+    ReactDOM.render(
+        <Provider store={store}>
+            <Index />
+        </Provider>,
+        document.getElementById("app")
+    );
+}
