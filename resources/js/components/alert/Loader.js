4a6dcbd81b4f1339ad34dadb3c7f8b4a20c76a3c
Liviu Andrei <liviuandrei.dev@gmail.com>
2021-09-07 17:24:53 +0300

    Add alert react components
    	+ Loader component its a simple circular spinner
    	+ Message component its an alert that displays a message with dynamic text and variant (different colors i.e. error/success/info etc..)

---
+++ resources/js/components/alert/Loader.js
@@ -0,0 +1,10 @@
+import { CircularProgress } from '@material-ui/core'
+import React from 'react'
+
+const Loader = () => {
+    return (
+        <CircularProgress />
+    )
+}
+
+export default Loader
