80b22207ce942e9288446b9affa95aa484338cc0
Liviu Andrei <liviuandrei.dev@gmail.com>
2021-09-14 15:32:01 +0300

    Add address requests & resources class files

---
+++ app/Http/Requests/address/AddressStoreRequest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Requests\address;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class AddressStoreRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'user_id'       => 'required|numeric',
+            'name'          => 'string|required|max:50',
+            'surname'       => 'string|required|max:50',
+            'country'       => 'string|required|max:50',
+            'city'          => 'string|required|max:50',
+            'address'       => 'string|required|max:170',
+
+            'postal_code'   => 'required|numeric',
+            'phone_number'  => 'required',
+        ];
+    }
+}
---
+++ app/Http/Resources/address/AddressIndexResource.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Resources\address;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class AddressIndexResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'id'            => $this->id,
+            'user_id'       => $this->user_id,
+            'name'          => $this->name,
+            'surname'       => $this->surname,
+            'country'       => $this->country,
+            'city'          => $this->city,
+            'address'       => $this->address,
+            'postal_code'   => $this->postal_code,
+            'phone_number'  => $this->phone_number,
+
+            'user'          => $this->user,
+        ];
+    }
+}
