bf6277b64efa7c22bf2d539d4ab457aa59010d72
Liviu Andrei <liviuandrei.dev@gmail.com>
2021-09-14 15:32:26 +0300

    Add address model class and migration file

---
+++ database/migrations/2021_09_13_124317_create_addresses_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAddressesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('addresses', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->string('name');
+            $table->string('surname');
+            $table->string('country');
+            $table->string('city');
+            $table->text('address');
+            $table->string('postal_code');
+            $table->string('phone_number');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('addresses');
+    }
+}
