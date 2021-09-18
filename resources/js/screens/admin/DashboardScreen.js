534cb48c76a286e17f4a878cb931c8a653baae09
Liviu Andrei <liviuandrei.dev@gmail.com>
2021-09-18 16:09:43 +0300

    Add dashboard screen

--- resources/js/screens/admin/DashboardScreen.js
+++ resources/js/screens/admin/DashboardScreen.js
@@ -3,6 +3,11 @@ import { useDispatch, useSelector } from "react-redux";
 import { Paper } from "@material-ui/core";
 import Loader from "../../components/alert/Loader";
 import { makeStyles } from "@material-ui/core/styles";
+import OrderCountChartScreen from "./charts/OrderCountChartScreen";
+import RevenueLastMonthScreen from "./charts/RevenueLastMonthScreen";
+import RevenueAllTimeScreen from "./charts/RevenueAllTimeScreen";
+import AverageRevenueScreen from "./charts/AverageRevenueScreen";
+import UserCountChartScreen from "./charts/UserCountChartScreen";
 
 const useStyles = makeStyles((theme) => ({
     divider: {
@@ -16,8 +21,8 @@ const useStyles = makeStyles((theme) => ({
             margin: "0 auto 20px auto",
         },
 
-        color: '#855C1B',
-        fontFamily: 'Quicksand'
+        color: "#855C1B",
+        fontFamily: "Quicksand",
     },
 }));
 
@@ -26,6 +31,7 @@ const DashboardScreen = ({ history }) => {
     const dispatch = useDispatch();
 
     const [isAdmin, setIsAdmin] = useState(false);
+    const [ordersCount, setOrdersCount] = useState([]);
 
     const userLogin = useSelector((state) => state.userLogin);
     const { userInfo } = userLogin;
@@ -35,8 +41,23 @@ const DashboardScreen = ({ history }) => {
             history.push("/login");
         } else {
             setIsAdmin(true);
+
+            const config = {
+                headers: {
+                    Authorization: `Bearer ${userInfo.data.access_token}`,
+                },
+            };
+
+            const promise1 = axios.get("/api/orderCharts", config);
+
+            async function fetchData() {
+                const response = await Promise.all([promise1]);
+
+                setOrdersCount(response[0].data);
+            }
+            fetchData();
         }
-    }, [userInfo]);
+    }, [dispatch]);
 
     return (
         <>
@@ -48,6 +69,28 @@ const DashboardScreen = ({ history }) => {
                 <>
                     <Paper className="admin-content">
                         <h2 className={classes.divider}>Dashboard</h2>
+
+                        <div className="chart">
+                            <div className="chart--widgets">
+                                <RevenueLastMonthScreen
+                                    revenue={ordersCount.revenueLastMonth}
+                                />
+                                <RevenueAllTimeScreen
+                                    revenue={ordersCount.revenueAllTime}
+                                />
+                                <AverageRevenueScreen
+                                    revenue={ordersCount.averageRevenue}
+                                />
+                            </div>
+                            <div className="chart--counts">
+                                <OrderCountChartScreen
+                                    orders={ordersCount.orderCount}
+                                />
+                                <UserCountChartScreen
+                                    users={ordersCount.userCount}
+                                />
+                            </div>
+                        </div>
                     </Paper>
                 </>
             )}
