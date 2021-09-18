c1ccf30321b0252792311ace68425b85ea54fd1b
Liviu Andrei <liviuandrei.dev@gmail.com>
2021-09-18 18:24:53 +0300

    Remove sweetalert from login

--- resources/js/screens/public/users/LoginScreen.js
+++ resources/js/screens/public/users/LoginScreen.js
@@ -104,23 +104,6 @@ const LoginScreen = ({ location, history }) => {
         e.preventDefault();
 
         dispatch(login(email, password, rememberMe));
-
-        const Toast = Swal.mixin({
-            toast: true,
-            position: "top-end",
-            showConfirmButton: false,
-            timer: 3000,
-            timerProgressBar: true,
-            didOpen: (toast) => {
-                toast.addEventListener("mouseenter", Swal.stopTimer);
-                toast.addEventListener("mouseleave", Swal.resumeTimer);
-            },
-        });
-
-        Toast.fire({
-            icon: "success",
-            title: "Signed in successfully",
-        });
     };
 
     return (
