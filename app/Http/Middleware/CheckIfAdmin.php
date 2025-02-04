b3a6e807e0573efcf174d2083b0e33b105dd1d4f
Liviu <liviuandrei.dev@gmail.com>
2025-02-04 14:24:32 +0200

    small cleanup

--- app/Http/Middleware/CheckIfAdmin.php
+++ app/Http/Middleware/CheckIfAdmin.php
@@ -11,32 +11,22 @@ class CheckIfAdmin
     /**
      * Handle an incoming request.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
+     * @param Request $request
+     * @param Closure $next
      * @return mixed
      */
-    public function handle(Request $request, Closure $next)
+    public function handle(Request $request, Closure $next): mixed
     {
-        // check if user is logged in
         if (Auth::check()) {
-
-            // loop through user's roles
             foreach(Auth::user()->roles as $role) {
-
-                // check if user role has admin permissions
                 if ($role->is_admin > 0) {
-
-                    // continue if true
                     return $next($request);
-
-                // else redirect to '/' with warning
                 } else {
                     return redirect('/')->with('danger', "You don't have permission to continue");
                 }
             }
         }
 
-        // redirect to login if user is not logged in
         return redirect('/login');
     }
 }
