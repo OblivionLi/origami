2a132b0930988db5d82b9675955e4afd3843122b
Liviu <liviuandrei.dev@gmail.com>
2025-03-06 15:25:18 +0200

    fixed some uses with steps ci

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,17 +1,16 @@
 #!/bin/sh
 
-# Generate the application key if it's not set (production)
-if [ -z "$APP_KEY" ] || [ "$APP_KEY" = $(php -r "echo env('APP_KEY', 'base64:HnNZ6akS52s9hiL5KKokZcYsFiABACY/JJwXqLaA+k0=');") ]; then
-    echo "Generating application key..."
-    php artisan key:generate --force --ansi
+set -e
+
+# Generate the application key if it doesn't exist
+if [ ! -f .env ]; then
+  echo "Creating .env file..."
+  cp .env.example .env
 fi
 
-# Run any migrations (optional)
-#php artisan migrate --force
+if [ -z "$(grep '^APP_KEY=' .env | cut -d= -f2)" ]; then
+    echo "Generating application key..."
+    php artisan key:generate --force
+fi
 
-# Ensure permissions are correct (This is already done in the Dockerfile)
-#chown -R www-data:www-data /var/www/html/storage /var/www/html/bootstrap/cache
-#chmod -R 775 /var/www/html/storage /var/www/html/bootstrap/cache
-
-# Execute the command passed to the container
 exec "$@"
